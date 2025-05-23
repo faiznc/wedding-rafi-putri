42122f2b111d90f6bda522c95ec6579f5af37739
Faiz Noerdiyan Cesara <faiz.cesara@formulatrix.com>
2025-05-23 10:19:18 +0700

    all changes , go to different machine

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // fitur Hitung Mundur
 // Mengatur waktu akhir perhitungan mundur
 // TODO : Change the desired date!
-var countDownDate = new Date("Apr 25, 2025 10:00:00").getTime();
+var countDownDate = new Date("June 26, 2025 11:00:00").getTime();
 
 // Memperbarui hitungan mundur setiap 1 detik
 var x = setInterval(function () {
